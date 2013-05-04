8d012d93bf151de8c620fd5d9b868e46e272e738
PeterOelsted <peter.oe@gmail.com>
2013-05-04 17:13:47 +0200

    New pictures Window now centered

--- ExiledRTS/ExiledRTS/Util/ExtenstionClass.cs
+++ ExiledRTS/ExiledRTS/Util/ExtenstionClass.cs
@@ -56,4 +56,24 @@ public static class ExtenstionClass
 
         spriteBatch.Draw(texture, position, texture.Bounds, Color.White, 0.0f, Vector2.Zero, 1.0f, flipped ? SpriteEffects.FlipHorizontally : SpriteEffects.None, depth);
     }
+
+    public static void SetPosition(this GameWindow window, Point position)
+    {
+         OpenTK.GameWindow OTKWindow = GetForm(window);
+         if (OTKWindow != null)
+         {
+             OTKWindow.X = position.X;
+             OTKWindow.Y = position.Y;
+         }
+
+     }
+
+    public static OpenTK.GameWindow GetForm(this GameWindow gameWindow)
+    {
+        Type type = typeof(OpenTKGameWindow);
+        System.Reflection.FieldInfo field = type.GetField("window", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+        if (field != null)
+            return field.GetValue(gameWindow) as OpenTK.GameWindow;
+        return null;
+    }
 }
